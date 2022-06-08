f4f6ab271ccda35a239fbcbdee66ef441c5b27a2
DimucaTheDev <74795935+DimucaTheDev@users.noreply.github.com>
2022-06-09 00:46:26 +0300

    1.19

--- src/main/java/com/dimucathedev/shaftcraft/Blocks/Deco.java
+++ src/main/java/com/dimucathedev/shaftcraft/Blocks/Deco.java
@@ -52,7 +52,7 @@ public class Deco extends FallingBlock {
             return InteractionResult.CONSUME;
         }
     }
-CraftingContainer
+
     public MenuProvider getMenuProvider(BlockState p_52240_, Level p_52241_, BlockPos p_52242_) {
         return new SimpleMenuProvider((p_52229_, p_52230_, p_52231_) -> {
             return new CraftingMenu(p_52229_, p_52230_, ContainerLevelAccess.create(p_52241_, p_52242_));
