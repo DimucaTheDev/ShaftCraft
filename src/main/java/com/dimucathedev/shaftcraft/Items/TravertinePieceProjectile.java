9352fd2bf7a867e408fb15229cb86c26dd9b5502
DimucaTheDev <74795935+DimucaTheDev@users.noreply.github.com>
2022-06-10 01:14:13 +0300

    ф

--- src/main/java/com/dimucathedev/shaftcraft/Items/TravertinePieceProjectile.java
+++ src/main/java/com/dimucathedev/shaftcraft/Items/TravertinePieceProjectile.java
@@ -22,6 +22,7 @@ public class TravertinePieceProjectile extends Item {
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
         p_41423_.add(Component.translatable("tooltip.travertine_throwable1"));
         p_41423_.add(Component.translatable("tooltip.travertine_throwable2"));
+        p_41423_.add(Component.translatable("tooltip.travertine_glass_break"));
         p_41423_.add(Component.translatable("tooltip.cooldown.travertine_piece"));
     }
 
